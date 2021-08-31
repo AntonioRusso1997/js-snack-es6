aa4a1e840e2be75879332b7c586e1ca8d01f4e28
Antonio <antonioru1997@gmail.com>
2021-08-31 13:58:02 +0200

    Snack 1 no bonus

--- js/main.js
+++ js/main.js
@@ -4,10 +4,39 @@ Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e pe
 Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 BONUS: inserire una arrow function che preso in input l'array di bici ritorni l'oggetto con bici più leggera. */
 
+// Creo array di oggetti
+let bikes = [
+    {
+        name: "Canyon Ultimate",
+        weight: 6.7
+    },
+    {
+        name: "Rose X-Lite",
+        weight: 6.9
+    },
+    {
+        name: "Sarto asola",
+        weight: 5.7
+    },
+    {
+        name: "Wilier Triestina Zero",
+        weight: 6.1
+    }    
+];
 
+//Stampo a schermo la bici con il peso minore
+let lowerWeight = bikes[0];
+
+for (let i = 0; i < bikes.length; i++) {
+
+    if (bikes[i].weight < lowerWeight.weight) {
+        lowerWeight = bikes[i];
+    }
+};
+
+console.log(lowerWeight);
 
 /* --------------------------------------------------*/
-
 
 /* Snack 2:
 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
